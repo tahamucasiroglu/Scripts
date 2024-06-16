86e251a2f8d47c17caf42e6519f1a363aac7b5d6
TahaMucasiroglu <tahamucasiroglu@gmail.com>
2024-06-17 00:45:07 +0300

    ubuntu için başlangıç kurulum listesi

---
+++ UbuntuFirstInstall.py
@@ -0,0 +1,37 @@
+import os
+os.system("sudo sysctl -w net.ipv6.conf.all.disable_ipv6=1")
+os.system("sudo sysctl -w net.ipv6.conf.default.disable_ipv6=1")
+os.system("sudo sysctl -w net.ipv6.conf.lo.disable_ipv6=1")
+os.system("sudo apt-get update --yes")
+os.system("sudo apt-get upgrade --yes")
+
+
+os.system("sudo apt install python3-pip --yes") # python pip kurulum 
+os.system("sudo apt-get install python3-tk --yes") # python tkinter kurma
+os.system("pip install pyqt5 pyqt5-tools --yes") # pyqt5 kurulum
+os.system("sudo apt install libgl1-mesa-glx --yes")
+os.system("sudo apt install libegl1-mesa libxkbcommon-x11-0 --yes")
+os.system("sudo apt install libxcb-xinerama0 libxcb1 libxcb-util1 libx11-xcb1 libxrender1 libxi6 --yes")
+os.system("sudo apt install qt5-default --yes")
+os.system("sudo apt install qtbase5-dev qtbase5-dev-tools --yes")
+os.system("sudo apt install libxkbcommon-x11-0 --yes")
+os.system("sudo apt install qt5-qpa-plugins --yes")
+os.system("pip install requests")
+os.system("pip install flask")
+os.system("")
+os.system("")
+os.system("")
+os.system("")
+os.system("")
+os.system("")
+os.system("")
+os.system("")
+os.system("")
+os.system("")
+os.system("")
+os.system("")
+os.system("")
+os.system("")
+
+os.system("sudo apt-get update --yes")
+os.system("sudo apt-get upgrade --yes")
